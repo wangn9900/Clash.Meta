3d6882a6f4c7a903705d745381d125fe36fe8cec
tianquege <wangn5635@gmail.com>
2026-05-09 03:44:03 +0800

    feat: modify UrlTestHook to allow latency suppression

--- adapter/patch.go
+++ adapter/patch.go
@@ -1,5 +1,5 @@
 package adapter
 
-type UrlTestCheck func(url string, name string, delay uint16)
+type UrlTestCheck func(url string, name string, delay uint16) uint16
 
 var UrlTestHook UrlTestCheck
